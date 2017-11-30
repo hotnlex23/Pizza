c928a057577d34697f47c02f0206ebf90ec0e046
hotnlex23 <32115959+hotnlex23@users.noreply.github.com>
2017-11-29 19:35:09 -0500

    Add files via upload

--- pizza.py
+++ pizza.py
@@ -20,7 +20,7 @@ class Pizza:
         
 
 
-        Label(frame1, text = "Pizza Shop").grid(row = 1, column = 3)
+        Label(frame1, text = "Pizza Shop").grid(row = 1, column = 3, pady = 20)
 
         frame2 = Frame(window)
         frame2.pack()
@@ -30,13 +30,13 @@ class Pizza:
 
         Label(frame2, text = "Choose Pizza Size:").grid(row = 1, column = 1, sticky = W)
         
-        Radiobutton(frame2, text = "Small",variable = self.v1, value = 1).grid(row = 2, column = 2,sticky = E)
+        Radiobutton(frame2, text = "Small",variable = self.v1, value = 1).grid(row = 2, column = 2,sticky = W)
 
         
-        Radiobutton(frame2, text = "Medium",variable = self.v1, value = 2).grid(row = 3, column = 2,sticky = E)
+        Radiobutton(frame2, text = "Medium",variable = self.v1, value = 2).grid(row = 3, column = 2,sticky = W)
 
        
-        Radiobutton(frame2, text = "Large",variable = self.v1, value = 3).grid(row = 4, column = 2,sticky = E)
+        Radiobutton(frame2, text = "Large",variable = self.v1, value = 3).grid(row = 4, column = 2,sticky = W)
 
 
 
@@ -45,13 +45,13 @@ class Pizza:
 
         Label(frame2, text = "Choose Pizza Crust:").grid(row = 1, column = 4)
         
-        Radiobutton(frame2, text = "Original",variable = self.v2, value = 1).grid(row = 2,padx = 6, column = 5)
+        Radiobutton(frame2, text = "Original",variable = self.v2, value = 1).grid(row = 2,padx = 6, column = 5, sticky = W)
 
         
-        Radiobutton(frame2, text = "Pan",variable = self.v2, value = 2).grid(row = 3,padx = 6, column = 5)
+        Radiobutton(frame2, text = "Pan",variable = self.v2, value = 2).grid(row = 3,padx = 6, column = 5, sticky = W)
 
        
-        Radiobutton(frame2, text = "Thin",variable = self.v2, value = 3).grid(row = 4,padx = 6, column = 5)
+        Radiobutton(frame2, text = "Thin",variable = self.v2, value = 3).grid(row = 4,padx = 6, column = 5, sticky = W)
 
 
         # create label for checkbutton
@@ -64,35 +64,35 @@ class Pizza:
         # create variables and checkbuttons for toppings
         self.pep = IntVar()
         self.pep.set(0)
-        Checkbutton(frame2, text = "Pepperoni", variable = self.pep).grid(row = 6, padx = 6,pady = 6,column = 2)
+        Checkbutton(frame2, text = "Pepperoni", variable = self.pep).grid(row = 6, padx = 6,pady = 6,column = 2, sticky = W)
         
         self.sausage = IntVar()
         self.sausage.set(0)
-        Checkbutton(frame2, text = "Sausage", variable = self.sausage).grid(row = 6,padx = 6,pady = 6, column = 3)
+        Checkbutton(frame2, text = "Sausage", variable = self.sausage).grid(row = 6,padx = 6,pady = 6, column = 3, sticky = W)
         
         self.ham = IntVar()
         self.ham.set(0)
-        Checkbutton(frame2, text = "Ham", variable = self.ham).grid(row = 6, padx = 6,pady = 6,column = 4)
+        Checkbutton(frame2, text = "Ham", variable = self.ham).grid(row = 6, padx = 6,pady = 6,column = 4, sticky = W)
         
         self.bacon = IntVar()
         self.bacon.set(0)
-        Checkbutton(frame2, text = "Bacon", variable = self.bacon).grid(row = 6,padx = 6,pady = 6, column = 5)
+        Checkbutton(frame2, text = "Bacon", variable = self.bacon).grid(row = 6,padx = 6,pady = 6, column = 5 , sticky = W)
         
         self.peppers = IntVar()
         self.peppers.set(0)
-        Checkbutton(frame2, text = "Peppers", variable = self.peppers).grid(row = 7,padx = 6,pady = 6, column = 2)
+        Checkbutton(frame2, text = "Peppers", variable = self.peppers).grid(row = 7,padx = 6,pady = 6, column = 2, sticky = W)
         
         self.olives = IntVar()
         self.olives.set(0)
-        Checkbutton(frame2, text = "Olives", variable = self.olives).grid(row = 7,padx = 6,pady = 6, column = 3)
+        Checkbutton(frame2, text = "Olives", variable = self.olives).grid(row = 7,padx = 6,pady = 6, column = 3, sticky = W)
         
         self.mush = IntVar()
         self.mush.set(0)
-        Checkbutton(frame2, text = "Mushrooms", variable = self.mush).grid(row = 7,padx = 6,pady = 6, column = 4)
+        Checkbutton(frame2, text = "Mushrooms", variable = self.mush).grid(row = 7,padx = 6,pady = 6, column = 4, sticky = W)
         
         self.onions = IntVar()
         self.onions.set(0)
-        Checkbutton(frame2, text = "Onions", variable = self.onions).grid(row = 7,padx = 6,pady = 6, column = 5)
+        Checkbutton(frame2, text = "Onions", variable = self.onions).grid(row = 7,padx = 6,pady = 6, column =5 , sticky = W)
 
 
         # extra items checkboxes
@@ -101,18 +101,18 @@ class Pizza:
 
         
         # creat label for extra items
-        Label(frame2, text = "Extra Items:").grid(row = 9, column = 1)
+        Label(frame2, text = "Extra Items:").grid(row = 9, column = 1,sticky =  W)
 
         # create checkbuttons for extrA items
         self.bread = IntVar()
         self.bread.set(0)
-        Checkbutton(frame2, text = "Cheese Bread", variable = self.bread).grid(row = 10, padx = 6,pady = 6,column = 2)
+        Checkbutton(frame2, text = "Cheese Bread", variable = self.bread).grid(row = 10, padx = 6,pady = 26,column = 2)
         self.wings = IntVar()
         self.wings.set(0)
-        Checkbutton(frame2, text = "Wings", variable = self.wings).grid(row = 10,padx = 6,pady = 6, column = 3)
+        Checkbutton(frame2, text = "Wings", variable = self.wings).grid(row = 10,padx = 6,pady = 26, column = 3)
         self.rolls = IntVar()
         self.rolls.set(0)
-        Checkbutton(frame2, text = "Cinnamon Rolls", variable = self.rolls).grid(row = 10,padx = 6,pady = 6, column = 4)
+        Checkbutton(frame2, text = "Cinnamon Rolls", variable = self.rolls).grid(row = 10,padx = 6,pady = 26, column = 4)
 
 
 
@@ -131,14 +131,14 @@ class Pizza:
         
 
           # create discount button
-        #Button(frame2, text = "Senior Discount", command = self.calcDiscount).grid(row = 17, pady = 6, column = 1, sticky = W)
+        Button(frame2, text = "Discount", command = self.calcDiscount).grid(row = 17, pady = 6,padx = 6, column = 1, sticky = W)
 
         
 
         
        
 
-        
+        self.discount = StringVar()
 
       
         Label(frame2, text = "Discounted Total").grid(row = 14,padx = 6,pady = 6, column = 4)
@@ -169,15 +169,17 @@ class Pizza:
        
         
         
-                
+              
     def calcDiscount(self):
-            return self.discount
+            SENIOR = .10
+
+            total = self.calcTotal()
+
+            discount = total - (total * SENIOR)
+
+            self.discount.set(discount)
 
             
-            
-       
-       
-
         
 
 
